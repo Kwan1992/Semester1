4fa01409b3e929e824cbb214613109494c68b330
Kwanjai <phatcharaporn.soda@gmail.com>
2026-01-28 11:01:53 +0100

    First commit

---
+++ src/Lesson01/MyFirstJavaCode.java
@@ -0,0 +1,9 @@
+package Lesson01;
+
+public class MyFirstJavaCode {
+    public static void main(String[] args) {
+        System.out.print("Hello! This is my first Java Code! Jubbi!!");
+        System.out.println("This is going to be fun");
+
+    }
+}
